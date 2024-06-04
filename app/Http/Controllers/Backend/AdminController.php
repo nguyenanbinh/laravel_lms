dda457bcd6ff37f45bd2d04554463a6cc2a73394
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2024-06-04 16:50:22 +0700

    admin coupon crud

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
@@ -170,6 +171,34 @@ class AdminController extends Controller
         }
 
         return response()->json(['message' => 'User Status Updated Successfully']);
+
+    }// End Method
+
+    public function adminAllCourse()
+    {
+        $course = Course::latest()->get();
+        return view('admin.backend.courses.all_course',compact('course'));
+
+    }// End Method
+
+    public function updateCourseStatus(Request $request)
+    {
+        $courseId = $request->input('course_id');
+        $isChecked = $request->input('is_checked', 0);
+        $course = Course::find($courseId);
+
+        if ($course) {
+            $course->status = $isChecked;
+            $course->save();
+        }
+
+        return response()->json(['message' => 'Course Status Updated Successfully']);
+    }// End Method
+
+    public function adminCourseDetails($id){
+
+        $course = Course::find($id);
+        return view('admin.backend.courses.course_details',compact('course'));
 
     }// End Method
 }
