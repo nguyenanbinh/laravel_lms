62704ce6bd3b0bc3ca8b859014d0a08fb04a133d
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2024-06-05 07:48:55 +0700

    checkout and payment for order

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -80,11 +80,40 @@
 <script src="{{ asset('frontend/js/plyr.js') }}"></script>
 <script src="{{ asset('frontend/js/jquery.lazy.min.js') }}"></script>
 <script src="{{ asset('frontend/js/main.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-@include('frontend.body.script')
 
 <script>
     var player = new Plyr('#player');
 </script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ let type = "{{ Session::get('alert-type','info') }}"
+
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break;
+ }
+ @endif
+
+ console.log("{{ Session::get('message') }} ");
+</script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+@include('frontend.body.script')
+
 </body>
 </html>
